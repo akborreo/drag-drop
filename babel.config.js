b32f9965276d69a2041775c2cf1adc867b6ed8c5
annborreo <141047181+annborreo@users.noreply.github.com>
2024-06-25 23:55:22 +0800

    Init

---
+++ babel.config.js
@@ -0,0 +1,11 @@
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: ['babel-preset-expo'],
+    plugins: [
+      '@realm/babel-plugin',
+      ['@babel/plugin-proposal-decorators', { legacy: true }],
+      'react-native-reanimated/plugin',
+    ],
+  };
+};
